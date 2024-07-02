474ba8011141c16528ae260273c194f685d341e0
Joao Treto <joaoatreto@gmail.com>
2024-07-02 00:31:20 -0300

    Alterações solicitadas

--- test/app.test.mjs
+++ test/app.test.mjs
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'test';
 
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import app from '../app.js'; // Importando como exportação padrão
+import app from '../app.js';
 const { expect } = chai;
 
 chai.use(chaiHttp);
@@ -11,7 +11,7 @@ describe('Teste de CRUD', () => {
 
     // Teste para cadastro de fornecedor
     describe('POST /register', () => {
-        it('deve cadastrar um novo fornecedor', (done) => {
+        it('Novo fornecedor cadastrado', (done) => {
             const novoFornecedor = {
                 name: 'Fornecedor Teste',
                 cnpj: '12.345.678/0001-99',
@@ -34,7 +34,7 @@ describe('Teste de CRUD', () => {
 
     // Teste para buscar produtos
     describe('GET /produtos', () => {
-        it('deve retornar uma lista de produtos', (done) => {
+        it('Lista de produtos retornada', (done) => {
             chai.request(app)
                 .get('/produtos')
                 .end((err, res) => {
